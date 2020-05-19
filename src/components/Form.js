cfcec547883ca202e05f4f2321574043983620b4
Adam Ostrowicki <adamostrowicki11@gmail.com>
2020-05-19 22:31:30 +0200

    contact

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,8 +2,14 @@ import React from 'react'
 
 export default function Form() {
     return (
-        <div>
-            
-        </div>
+        <form action="" className="">
+            <label htmlFor="name">Name</label>
+            <input type="text" name="name" id="name" />
+            <label htmlFor="email">E-mail</label>
+            <input type="email" name="email" id="email" />
+            <label htmlFor="message">Message</label>
+            <textarea name="message" id="message" cols="30" rows="7"></textarea>
+            <input type="submit" value="Send" className="btn"/>
+        </form>
     )
 }
