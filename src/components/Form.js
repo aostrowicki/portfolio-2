2241da352be1cc49695da1a17754c017f61dd3ce
Adam Ostrowicki <adamostrowicki11@gmail.com>
2020-08-13 19:45:54 +0200

    .

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Form() {
     return (
-        <form method="POST" data-netlify="true" name="contact">
+        <form name="contact">
             <label htmlFor="name">Name</label>
             <input type="text" name="name" id="name" />
             <label htmlFor="email">E-mail</label>
