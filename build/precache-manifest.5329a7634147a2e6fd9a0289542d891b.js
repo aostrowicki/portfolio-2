2c92bcd41b7828fe7d79c3c49c0616ad58af881a
Adam Ostrowicki <adamostrowicki11@gmail.com>
2020-08-13 20:17:51 +0200

    build

---
+++ build/precache-manifest.5329a7634147a2e6fd9a0289542d891b.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03e9ad8abca2f4296f49938bb4bdd1d1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "49526335b712913afdc9",
+    "url": "/static/css/2.f656da75.chunk.css"
+  },
+  {
+    "revision": "683a84a3b3243b8141a9",
+    "url": "/static/css/main.cf58d1a6.chunk.css"
+  },
+  {
+    "revision": "49526335b712913afdc9",
+    "url": "/static/js/2.3b255df5.chunk.js"
+  },
+  {
+    "revision": "3d3565e03348b0968e72a9068f58f357",
+    "url": "/static/js/2.3b255df5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "683a84a3b3243b8141a9",
+    "url": "/static/js/main.906b4f35.chunk.js"
+  },
+  {
+    "revision": "f39dd5e0874fba7be551",
+    "url": "/static/js/runtime-main.10b11e94.js"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/static/media/Adam-Ostrowicki.d41d8cd9.pdf"
+  },
+  {
+    "revision": "5384fc2c9328922cfaca25749d03229f",
+    "url": "/static/media/about.5384fc2c.svg"
+  },
+  {
+    "revision": "7a305ff44ecbd09bbc8a323d52bb3242",
+    "url": "/static/media/arrow-right.7a305ff4.svg"
+  },
+  {
+    "revision": "f80f6fb0796347451ac303609cce36ec",
+    "url": "/static/media/barb.f80f6fb0.png"
+  },
+  {
+    "revision": "f302c97d2817a907b626539113d02d27",
+    "url": "/static/media/close.f302c97d.svg"
+  },
+  {
+    "revision": "a6f2cd279584d958f62f9e33dd85b8b5",
+    "url": "/static/media/contact.a6f2cd27.svg"
+  },
+  {
+    "revision": "32b7979b8485c1cce5f582a73b740921",
+    "url": "/static/media/des.32b7979b.svg"
+  },
+  {
+    "revision": "d54258c0c3941e445ca85a326bf95658",
+    "url": "/static/media/dev.d54258c0.svg"
+  },
+  {
+    "revision": "9e82138b76c771dcf27901a6bc8e4600",
+    "url": "/static/media/fiverr.9e82138b.svg"
+  },
+  {
+    "revision": "612f8feb0284edaef7d08f671f44fbd6",
+    "url": "/static/media/github.612f8feb.svg"
+  },
+  {
+    "revision": "121b210c8094a232de95c688339b2155",
+    "url": "/static/media/hero.121b210c.svg"
+  },
+  {
+    "revision": "ece871a1568ee29c2ac94836f62c506b",
+    "url": "/static/media/icons1.ece871a1.png"
+  },
+  {
+    "revision": "0661a7d5a6ee06088032d0a3c3f2ac2b",
+    "url": "/static/media/icons2.0661a7d5.png"
+  },
+  {
+    "revision": "8bd18a1052299d351684a02baefbd712",
+    "url": "/static/media/icons3.8bd18a10.png"
+  },
+  {
+    "revision": "f9ea7422a6cb25c23998a1910765320c",
+    "url": "/static/media/illu1.f9ea7422.png"
+  },
+  {
+    "revision": "3ce1d62ac9bcb49ed1d5abf0fcb3333c",
+    "url": "/static/media/illu2.3ce1d62a.png"
+  },
+  {
+    "revision": "653b3f67222bbd5736b3ba069820ae52",
+    "url": "/static/media/illustration.653b3f67.svg"
+  },
+  {
+    "revision": "01224f96c6b0bfaf282be5f0d0b3777d",
+    "url": "/static/media/linkedin.01224f96.svg"
+  },
+  {
+    "revision": "aa58ab9f9f4915b0aa54d54ebb6087e0",
+    "url": "/static/media/location.aa58ab9f.svg"
+  },
+  {
+    "revision": "56ca8b865146bb4f956f70278cc8f901",
+    "url": "/static/media/logo.56ca8b86.svg"
+  },
+  {
+    "revision": "68a47bff8e9c8b2273ae6d2887f41c4c",
+    "url": "/static/media/mail.68a47bff.svg"
+  },
+  {
+    "revision": "a18c6961a0f1067068648381840e0ba9",
+    "url": "/static/media/outdo.a18c6961.png"
+  },
+  {
+    "revision": "075d68c113dd5cdfe59d17290f5d918f",
+    "url": "/static/media/portfolio-2.075d68c1.png"
+  },
+  {
+    "revision": "f9f0d23f8e24711fcd2c0cbeb6104697",
+    "url": "/static/media/portfolio.f9f0d23f.png"
+  },
+  {
+    "revision": "3e83f85f40c675df5a5a55165cb4301e",
+    "url": "/static/media/scroll.3e83f85f.svg"
+  }
+]);
